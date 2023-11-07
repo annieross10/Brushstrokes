102981dcc385c52721ded090d851de4dd86fc262
Annie Ross <annieross10@outlook.com>
2023-11-07 11:12:02 +0000

    add style.css

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['brushstrokesby-annie.herokuapp.com', 'localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'brushstrokesbyannie.herokuapp.com', 'localhost', 'brushstrokesbyannie-b5c294aa0510.herokuapp.com']
 
 
 # Application definition
