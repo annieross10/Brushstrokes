f601897bf266c8d4e62d97478f21c6211b9ab4fc
Annie Ross <annieross10@outlook.com>
2023-11-07 01:51:04 +0000

    create artwork model

--- brushstrokes/models.py
+++ brushstrokes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    artist = models.CharField(max_length=100)
+    created_on = models.CharField(max_length=200)
+    description = models.TextField()
+    medium = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    size = models.CharField(max_length=50, default='') 
+    year_created = models.PositiveIntegerField(default=timezone.now().year) 
+    STATUS_CHOICES = (
+        ('For Sale', 'For Sale'),
+        ('SOLD', 'SOLD'),
+    )
+    sold = models.CharField(max_length=8, choices=STATUS_CHOICES, default='For Sale') 
+    slug = models.SlugField(unique=True)
+    status = models.IntegerField(default=1)
+    saved_by = models.ManyToManyField(User, related_name='saved_artworks', blank=True)
+    
+    def save(self, *args, **kwargs):
+        current_year = timezone.now().year
+        if self.year_created > current_year:
+            raise ValidationError("Year created cannot exceed the current year.")
+        super(Artwork, self).save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('artwork_detail', args=[str(self.slug)])
