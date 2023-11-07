674969e1083cbd141c9ab6901cac72157f5a9074
Annie Ross <annieross10@outlook.com>
2023-11-07 11:47:47 +0000

    update template settings

--- blog/settings.py
+++ blog/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
