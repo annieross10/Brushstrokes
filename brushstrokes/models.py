2d1dbf883c3a5bc48069c17b9655d1f251b50530
Annie Ross <annieross10@outlook.com>
2023-11-07 10:02:24 +0000

    add comment and form models

--- brushstrokes/models.py
+++ brushstrokes/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 
 class Artwork(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -34,3 +33,27 @@ class Artwork(models.Model):
     
     def get_absolute_url(self):
         return reverse('artwork_detail', args=[str(self.slug)])
+    
+
+class Comment(models.Model):
+    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.artwork}'
+    
+
+class ContactFormSubmission(models.Model):
+    name = models.CharField(max_length=255, default="Anonymous")
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Contact Form Submission by {self.name}"
