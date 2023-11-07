9c8aabff39bfcac473a3d4683e96964f2ca353e2
Annie Ross <annieross10@outlook.com>
2023-11-07 12:41:31 +0000

    add index image

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["DATABASE_URL"]="postgres://pbqzrays:XujJ8xGJes3Qf-KIkvXbTP8I-wjkFwD5@trumpet.db.elephantsql.com/pbqzrays"
+os.environ["DATABASE_URL"]="postgres://vxchbmxg:ITN8Ssj9fPdZxrrLgpYprXCxgKB1bF4w@surus.db.elephantsql.com/vxchbmxg"
 os.environ["SECRET_KEY"]="annie_secret_key101"
 os.environ["CLOUDINARY_URL"]="cloudinary://677365176387411:2k6hrCep_l40IQW_4rywoEsuqUI@dj7k2zipj"
 
