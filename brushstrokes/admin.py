4e8821ba2dac972dddc161a6bc669bf452a62da1
Annie Ross <annieross10@outlook.com>
2023-11-07 10:08:19 +0000

    update admin.py

--- brushstrokes/admin.py
+++ brushstrokes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin 
+from .models import Artwork, Comment, ContactFormSubmission
 
-# Register your models here.
+# Define the Artwork admin
+class ArtworkAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'artist', 'year_created', 'sold', 'created_on')
+    list_filter = ('year_created', 'sold')
+    search_fields = ('title', 'artist')
+    prepopulated_fields = {'slug': ('title',)}  
+    summernote_fields = ('description',)  
+
+admin.site.register(Artwork, ArtworkAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artwork', 'created_on', 'approved')
+    list_filter = ('approved',)
+    search_fields = ('name', 'artwork__title') 
+
+admin.site.register(Comment, CommentAdmin)
+
+class ContactFormSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email', 'message')
+
+admin.site.register(ContactFormSubmission, ContactFormSubmissionAdmin)
