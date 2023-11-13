bbb4da08e38a4d6b812eebf83a00983a47a2218c
Annie Ross <annieross10@outlook.com>
2023-11-13 19:36:18 +0000

    env.py gitignore

--- env.py
+++ env.py
@@ -1,6 +1,2 @@
 import os
 
-os.environ["DATABASE_URL"]="postgres://vxchbmxg:ITN8Ssj9fPdZxrrLgpYprXCxgKB1bF4w@surus.db.elephantsql.com/vxchbmxg"
-os.environ["SECRET_KEY"]="annie_secret_key101"
-os.environ["CLOUDINARY_URL"]="cloudinary://981975134523581:ydj4q7HxIg_bgEU27UCdPAfx4H0@dok8kgikd"
-
